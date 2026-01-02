b95ca8a658a7afbc216c38cb6615fe0c076682de
scaleoutSean <scaleoutsean@users.noreply.github.com>
2026-01-03 05:56:10 +0800

    Adjust indentation in test file

--- tests/unit/plugins/modules/test_na_elementsw_cluster_config.py
+++ tests/unit/plugins/modules/test_na_elementsw_cluster_config.py
@@ -98,7 +98,7 @@ class TestMyModule(unittest.TestCase):
         module_args = {}
         module_args.update(self.set_default_args())
         ntp_dict = {'set_ntp_info': {
-                'ntp_servers': ['1.1.1.1']}}
+            'ntp_servers': ['1.1.1.1']}}
         module_args.update(ntp_dict)
         set_module_args(module_args)
         my_obj = my_module()
